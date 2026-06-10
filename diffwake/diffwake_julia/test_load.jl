f838317ab793aaadb689839e8dd31c33ba07420c
Johnny Chan <129523730+Changtze@users.noreply.github.com>
2026-06-10 17:29:01 +0100

    cleanp

--- diffwake/diffwake_julia/test_load.jl
+++
@@ -1,5 +0,0 @@
-wpush!(LOAD_PATH, raw"E:\Aeronautics\fyp\DiffWake\diffwake\diffwake_julia")
-include(raw"E:\Aeronautics\fyp\DiffWake\diffwake\diffwake_julia\DiffWakeJulia.jl")
-using .DiffWakeJulia
-println("Module loaded successfully")
-println("Exported names: ", names(DiffWakeJulia))
